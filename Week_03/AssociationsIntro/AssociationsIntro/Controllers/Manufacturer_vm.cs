82b238c109a6a03a4b98a3bb710c9d5659dc3b3a
peteratseneca <peter.mcintyre@senecacollege.ca>
2015-09-21 18:25:23 -0400

    Associations, one-to-many

---
+++ Week_03/AssociationsIntro/AssociationsIntro/Controllers/Manufacturer_vm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// more...
+using System.ComponentModel.DataAnnotations;
+
+namespace AssociationsIntro.Controllers
+{
+    public class ManufacturerAdd
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        [Required, StringLength(50)]
+        public string Country { get; set; }
+
+        [Range(1850, 2200)]
+        public int YearStarted { get; set; }
+    }
+
+    public class ManufacturerBase : ManufacturerAdd
+    {
+        public int Id { get; set; }
+    }
+
+    public class ManufacturerWithVehicles : ManufacturerBase
+    {
+        public IEnumerable<VehicleBase> Vehicles { get; set; }
+    }
+
+    public class ManufacturerEdit
+    {
+        // Must include the object's identifier as a property
+        public int Id { get; set; }
+
+        // We can decide what other properties can be edited
+        // This is based on the use case, and the intent of the property's data
+        // For this use case, we have decided that only the "Name" property can be changed
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+    }
+}
