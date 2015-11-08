8cc6cc093b8d39080c50e45e6394c567218ebbf9
peteratseneca <peter.mcintyre@senecacollege.ca>
2015-11-08 08:30:36 -0500

    Templates and solutions

---
+++ Templates_and_solutions/IdentityServerV1/SecMS/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+// new...
+using Microsoft.AspNet.Identity.Owin;
+
+namespace SecMS.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/UserList
+        // Attention - This will fetch the list of registered users
+        public ActionResult UserList()
+        {
+            // Container to hold the user names
+            var userList = new List<string>();
+
+            // Get a reference to the application's user manager
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            // Go through the users, and extract their names
+            foreach (var user in userManager.Users)
+            {
+                userList.Add(user.UserName);
+            }
+
+            // Package it for the view
+            ViewBag.UserList = userList;
+
+            return View();
+        }
+
+    }
+
+}
---
+++ Templates_and_solutions/README.md
@@ -0,0 +1,21 @@
+### Templates and Solutions
+
+**Web API project v1**
+
+Visual Studio project template.  
+Includes topics up to and including Week 6 (repo pattern, link relations, handlers, documentation).  
+
+**Web API project v2**
+
+Based on the "Web API project v1" project template.  
+Includes Week 7 topics (media handling).  
+
+**IdentityServerV1**
+
+Used in Week 8.  
+Does not include claims processing.  
+
+**IdentityServerV2**
+
+Builds upon ...v1 above.  
+Includes claims processing.  
