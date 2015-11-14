dc57d895f57cee3904fd9421b133bb4fd62dabe2
peteratseneca <peter.mcintyre@senecacollege.ca>
2015-11-14 09:27:29 -0500

    Security, working with claims

--- Week_09/IdentityServerV2/SecMS/App_Start/Startup.Auth.cs
+++ Week_09/IdentityServerV2/SecMS/App_Start/Startup.Auth.cs
@@ -40,7 +40,7 @@ namespace SecMS
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromHours(6),
+                        validateInterval: TimeSpan.FromDays(14),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager, DefaultAuthenticationTypes.ApplicationCookie))
                 }
             });
@@ -56,7 +56,7 @@ namespace SecMS
                 // CHANGE THIS!!! to a value that you like better
                 // Use the TimeSpan methods to shop for a suitable time span
                 // (could be minutes, hours, days - whatever you want)
-                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(10),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
                 AllowInsecureHttp = true
             };
 
